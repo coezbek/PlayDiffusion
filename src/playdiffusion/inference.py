Fix off by one error

src/playdiffusion/inference.py
@@ -614,7 +614,7 @@ class PlayDiffusion():
 
         with torch.inference_mode():
             output_chunks = []
-            last_end_frame = -1
+            last_end_frame = 0 # last_end_frame is exclusive index of the last inpainted region
             for chunk in diffs:
                 start_frame = chunk.start_frame
                 end_frame = chunk.end_frame
@@ -636,9 +636,9 @@ class PlayDiffusion():
                 logger.debug(f"Text: {text}")
 
                 # save the previous unchanged audio tokens
-                if start_frame is not None and start_frame > last_end_frame + 1:
+                if start_frame is not None and start_frame > last_end_frame:
                     output_chunks.append(
-                        input_audio_tokens[:, last_end_frame + 1 : start_frame]
+                        input_audio_tokens[:, last_end_frame : start_frame]
                     )
                     logger.debug(f"Unchanged audio tokens shape: {output_chunks[-1].shape}")
                 if end_frame is not None:
@@ -712,8 +712,8 @@ class PlayDiffusion():
                 output_chunks.append(inpaint_audio_tokens)
 
         # save the remaining unchanged audio tokens and concatenate all chunks
-        if input_audio_tokens.shape[-1] > last_end_frame + 1:
-            output_chunks.append(input_audio_tokens[:, last_end_frame + 1 :])
+        if input_audio_tokens.shape[-1] > last_end_frame:
+            output_chunks.append(input_audio_tokens[:, last_end_frame:])
             logger.debug(f"Unchanged audio tokens shape: {output_chunks[-1].shape}")
         return torch.cat(output_chunks, dim=1)
 

src/playdiffusion/models/inpainter/masklm_text.py
@@ -140,8 +140,8 @@ class MaskGCT(nn.Module):
         rescale_cfg = 0.75,
         topk = 20,
         code = None,
-        start_frame = None,
-        end_frame = None
+        start_frame = None, # Inclusive
+        end_frame = None # Exclusive!
     ):
         """
         text_tokens: B, T
@@ -163,17 +163,17 @@ class MaskGCT(nn.Module):
             assert end_frame is not None
             T = code.size(-1)
             if start_frame > 0:
-                code_before = code[:, 0 : start_frame]
+                code_before = code[:, 0:start_frame]
             else:
                 start_frame = 0
                 code_before = None
 
-            if end_frame < T-1:
+            if end_frame < T:
                 code_after = code[:, end_frame:]
             else:
-                end_frame = T - 1
+                end_frame = T
                 code_after = None
-            target_len = end_frame - start_frame + 1
+            target_len = end_frame - start_frame
         else:       # TTS
             assert start_frame is None and end_frame is None
             code_before = None